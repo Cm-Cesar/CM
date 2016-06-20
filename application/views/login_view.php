286f37dd82fc8228be32f1340f1d9db3bdbe6ec7
Cm-Cesar <csrmnol@gmail.com>
2016-06-19 23:03:21 -0600

    primer commit

---
+++ application/views/login_view.php
@@ -0,0 +1,27 @@
+<div class="container-fluid">
+	<div id="page-login" class="row">
+		<div class="col-xs-12 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+			<div class="text-right">
+				<a href="page_register.html" class="txt-default">Nesecitas una cuenta?</a>
+			</div>
+			<div class="box">
+				<div class="box-content">
+					<div class="text-center">
+						<h3 class="page-header">Control Maquinero</h3>
+					</div>
+					<div class="form-group">
+						<label class="control-label">Usuario</label>
+						<input type="text" class="form-control" name="username" />
+					</div>
+					<div class="form-group">
+						<label class="control-label">Contraseña</label>
+						<input type="password" class="form-control" name="password" />
+					</div>
+					<div class="text-center">
+						<a href="../index.html" class="btn btn-primary">Entrar</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
